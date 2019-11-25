update fixed point calculation

dynamo/plot/scVectorField.py
@@ -280,7 +280,7 @@ def grid_velocity(adata, genes, x=0, y=1, method='SparseVFC', basis='trimap', n_
     n_columns, plot_per_gene = n_columns, 1 # we may also add random velocity results
     nrow, ncol = int(np.ceil(plot_per_gene * n_genes / n_columns)), n_columns
     if figsize is None:
-        plt.figure(None, (3*ncol, 3*nrow)) # , dpi=160
+        plt.figure(None, (3*ncol, 3*nrow), dpi=160) #
     else:
         plt.figure(None, (figsize[0]*ncol, figsize[1]*nrow)) # , dpi=160
 
@@ -461,7 +461,7 @@ def stremline_plot(adata, genes, x=0, y=1, method='sparseVFC', basis='trimap', n
     n_columns, plot_per_gene = n_columns, 1 # we may also add random velocity results
     nrow, ncol = int(np.ceil(plot_per_gene * n_genes / n_columns)), n_columns
     if figsize is None:
-        plt.figure(None, (3*ncol, 3*nrow)) # , dpi=160
+        plt.figure(None, (3*ncol, 3*nrow), dpi=160) #
     else:
         plt.figure(None, (figsize[0]*ncol, figsize[1]*nrow)) # , dpi=160
 

dynamo/plot/topology.py
@@ -284,25 +284,42 @@ def topography(adata, basis, xlim, ylim, t=None, terms=['streamline', 'nullcline
         ax = plot_flow_field(ax, VF, xlim, ylim)
 
     if 'nullcline' in terms  or 'fixed_points' in terms or 'separatrices' in terms:
-        fix_points_dict = {"auto_func": None, "dim_range": xlim, "RandNum": 5000, "EqNum": 2, "x_ini": None}
+        fix_points_dict = {"auto_func": None, "dim_range": xlim, "RandNum": 5000, "EqNum": 2, "x_ini": None, "reverse": False, "grid_num": 50}
 
         if fixed_points_kwargs is not None:
             fix_points_dict.update(fixed_points_kwargs)
 
-        stable, saddle = gen_fixed_points(func=VF, **fix_points_dict)
+        # reverse vector field for identifying repellers
+        if fix_points_dict['reverse']:
+            from scipy.spatial.distance import cdist
+
+            stable_r, saddle_r = gen_fixed_points(func=VF, **fix_points_dict)
+            fix_points_dict['reverse'] = False
+            stable_f, saddle_f = gen_fixed_points(func=VF, **fix_points_dict)
+
+            # remove duplicated points
+            tmp = cdist(stable_r.T, stable_f.T)
+            stable_f = stable_f[:, list(set(np.arange(stable_f.shape[1])).difference(np.where(tmp < 1e-15)[1]))]
+            tmp = cdist(saddle_r.T, saddle_f.T)
+            saddle_f = saddle_f[:, list(set(np.arange(saddle_f.shape[1])).difference(np.where(tmp < 1e-15)[1]))]
+
+            stable, saddle = np.hstack((stable_r, stable_f)), np.hstack((saddle_r, saddle_f))
+        else:
+            stable, saddle = gen_fixed_points(func=VF, **fix_points_dict)
+
         stable = stable[:, np.logical_and(stable[0] >= xlim[0], stable[0] <= xlim[1]) &
                            np.logical_and(stable[1] >= ylim[0], stable[1] <= ylim[1])]
         saddle = saddle[:, np.logical_and(saddle[0] >= xlim[0], saddle[0] <= xlim[1]) &
                            np.logical_and(saddle[1] >= ylim[0], saddle[1] <= ylim[1])]
-
-    if 'nullcline' in terms:
         points = np.hstack((stable, saddle))
+
+    if 'nullcline' in terms and points.shape[1] > 0:
 
         ax = plot_null_clines(ax, VF, xlim, ylim, fixed_points=points)
 
-    if 'fixed_points' in terms:
+    if 'fixed_points' in terms and points.shape[1] > 0:
         ax = plot_fixed_points(ax, VF, dim_range=xlim, saddle=saddle, stable=stable)
-    if 'separatrices' in terms:
+    if 'separatrices' in terms and saddle.shape[1] > 0:
         if stable.shape[1] > 2:
             ax = plot_flow_field(ax, VF, xlim, ylim, start_points=saddle.T - 0.01)
             ax = plot_flow_field(ax, VF, xlim, ylim, start_points=saddle.T + 0.01)

dynamo/simulation/example_networks.py
@@ -23,11 +23,13 @@ def Ying_model(x, t=None):
     if len(x.shape) == 2:
         dx1 = -1 + 9 * x[:, 0] - 2 * pow(x[:, 0], 3) + 9 * x[:, 1] - 2 * pow(x[:, 1], 3)
         dx2 = 1 - 11*x[:, 0] + 2 * pow(x[:, 0], 3) + 11 * x[:, 1] - 2 * pow(x[:, 1], 3)
+
+        ret = np.array([dx1, dx2]).T
     else:
         dx1 = -1 + 9 * x[0] - 2 * pow(x[0], 3) + 9 * x[1] - 2 * pow(x[1], 3)
         dx2 = 1 - 11*x[0] + 2 * pow(x[0], 3) + 11 * x[1] - 2 * pow(x[1], 3)
 
-    ret = np.array([[dx1], [dx2]]).reshape(x.shape)
+        ret = np.array([dx1, dx2])
 
     return ret
 
@@ -104,23 +106,25 @@ def neurogenesis(x, t = None,
     return dx
 
 
-def state_space_sampler(ode, dim, min_val=0, max_val=4, N=10000): 
+def state_space_sampler(ode, dim, clip=True, min_val=0, max_val=4, N=10000):
     """Sample N points from the dim dimension gene expression space while restricting the values to be between min_val and max_val. Velocity vector at the sampled points will be calculated according to ode function.
     """
 
     X = np.array([ [uniform(min_val, max_val) for _ in range(dim)] for _ in range(N) ])
-    Y = np.clip( X + ode(X), a_min=min_val, a_max=None)
+    Y = np.clip( X + ode(X), a_min=min_val, a_max=None) if clip else X + ode(X)
 
     return X, Y
 
 
-def Simulator(motif='neurogenesis'):
+def Simulator(motif='neurogenesis', clip=True):
     """Simulate the gene expression dynamics via deterministic ODE model
 
     Parameters
     ----------
     motif: `str` (default: `neurogenesis`)
         Name of the network motif that will be used in the simulation.
+    clip: `bool` (default: `True`)
+        Whether to clip data points that are negative.  
 
     Returns
     -------
@@ -144,7 +148,7 @@ def Simulator(motif='neurogenesis'):
         gene_name = np.array(['Pu.1', 'Gata.1'])
     elif motif is 'Ying':
         cell_num = 5000
-        X, Y = state_space_sampler(ode=Ying_model, dim=2, min_val=-2, max_val=3, N=cell_num)
+        X, Y = state_space_sampler(ode=Ying_model, dim=2, clip=clip, min_val=-1, max_val=3, N=cell_num)
         gene_name = np.array(['X', 'Y'])
 
     var = pd.DataFrame({'gene_short_name': gene_name})  # use the real name in simulation?
@@ -160,21 +164,21 @@ def Simulator(motif='neurogenesis'):
     adata = AnnData(X.copy(), obs.copy(), var.copy(), layers=layers.copy())
 
     # remove cells that has no expression
-    adata = adata[adata.X.sum(1) > 0, :]
+    adata = adata[adata.X.sum(1) > 0, :] if clip else adata
 
     return adata
 
 
 if __name__ is '__main__':
     import dynamo as dyn
-    # toggle_adata = dyn.sim.Simulator(motif='toggle')
-    # dyn.tl.VectorField(toggle_adata, basis='X', velocity_key='velocity')
-    #
-    # dyn.pl.topography(toggle_adata, VF=None, basis='X', init_state=None, t=np.linspace(0, 10, 200),
-    #                   xlim=[0, 6], ylim=[0, 6], plot=True)
-    #
-    two_genes_adata = dyn.sim.Simulator(motif='twogenes')
-    dyn.tl.VectorField(two_genes_adata, basis='X', velocity_key='velocity')
+    toggle_adata = dyn.sim.Simulator(motif='toggle')
+    dyn.tl.VectorField(toggle_adata, basis='X', velocity_key='velocity')
+
+    dyn.pl.topography(toggle_adata, VF=None, basis='X', init_state=None, t=np.linspace(0, 10, 200),
+                      xlim=[0, 6], ylim=[0, 6], plot=True)
+
+    # two_genes_adata = dyn.sim.Simulator(motif='twogenes')
+    # dyn.tl.VectorField(two_genes_adata, basis='X', velocity_key='velocity')
     #
     # dyn.pl.topography(two_genes_adata, VF=None, basis='X', init_state=None, t=np.linspace(0, 10, 200),
     #                   xlim=[0, 6], ylim=[0, 6], plot=True)
@@ -182,4 +186,9 @@ if __name__ is '__main__':
     # adata=dyn.read_h5ad('/Volumes/xqiu/proj/Aristotle/backup/vector_field_hippocampus.h5ad')
     # dyn.pl.topography(adata, VF=None, basis='X', init_state=None, t=None,
     #                   xlim=[-27, 27], ylim=[-27, 27], plot=True)
-    dyn.pl.topography(two_genes_adata, VF=dyn.sim.two_genes_motif, basis='X', init_state=None, t=None, xlim=[0, 6], ylim=[0, 6], plot=True)
+    # dyn.pl.topography(two_genes_adata, VF=dyn.sim.two_genes_motif, basis='X', init_state=None, t=None, xlim=[0, 6], ylim=[0, 6], plot=True)
+    # Ying_adata = dyn.sim.Simulator(motif='Ying', clip=False)
+    # dyn.tl.VectorField(Ying_adata, basis='X', velocity_key='velocity', M=500, beta=2)
+    #
+    # dyn.pl.topography(Ying_adata, VF=None, basis='X', init_state=None, t=None,
+    #                   xlim=[-3, 3], ylim=[-3, 3], plot=True, reverse=True)

dynamo/tools/scPotential.py
@@ -11,7 +11,7 @@ from .Wang import Wang_action, Wang_LAP
 
 # the LAP method should be rewritten in TensorFlow/PyTorch using optimization with SGD
 
-def gen_fixed_points(func, auto_func, dim_range, RandNum, EqNum, x_ini = None):
+def gen_fixed_points(func, auto_func, dim_range, RandNum, EqNum, reverse=False, grid_num=50, x_ini = None):
     """ Calculate the fixed points of (learned) vector field function . Classify the fixed points into classes of stable and saddle points
     based on the eigenvalue of the Jacobian on the point.
 
@@ -19,7 +19,7 @@ def gen_fixed_points(func, auto_func, dim_range, RandNum, EqNum, x_ini = None):
     ---------
         func: 'function'
             The function of the (learned) vector field function that are required to fixed points for
-        auto_func: 'np.ndarray'
+        auto_func: 'np.ndarray' (not used)
             The function that is written with autograd of the same ODE equations that is used to calculate the Jacobian matrix.
             If auto_func is set to be None, Jacobian is calculated through the fjac, r returned from fsolve.
         dim_range: 'list'
@@ -28,6 +28,10 @@ def gen_fixed_points(func, auto_func, dim_range, RandNum, EqNum, x_ini = None):
             The number of random initial points to sample
         EqNum: 'int'
             The number of equations (dimension) of the system
+        reverse: `bool`
+            Whether to reverse the sign (direction) of vector field (VF).
+        grid_num: `int` (default: 50)
+            The number of grids on each dimension, only used when the EqNum is 2 and x_ini is None.
         x_ini: 'np.ndarray'
             The user provided initial points that is used to find the fixed points
 
@@ -41,9 +45,18 @@ def gen_fixed_points(func, auto_func, dim_range, RandNum, EqNum, x_ini = None):
     """
     import numdifftools as nda
 
+    if reverse is True :
+        func_ = lambda x: -func(x)
+    else:
+        func_ = func
     ZeroConst = 1e-8
     FixedPointConst = 1e-20
     MaxSolution = 1000
+
+    if x_ini is None and EqNum < 4:
+        _min, _max = [dim_range[0]] * EqNum, [dim_range[1]] * EqNum
+        Grid_list = np.meshgrid(*[np.linspace(i, j, grid_num) for i, j in zip(_min, _max)])
+        x_ini = np.array([i.flatten() for i in Grid_list]).T
 
     RandNum = RandNum if x_ini is None else x_ini.shape[0] # RandNum set to the manually input steady state estimates
     FixedPoint = np.zeros((RandNum, EqNum))
@@ -59,7 +72,7 @@ def gen_fixed_points(func, auto_func, dim_range, RandNum, EqNum, x_ini = None):
     if x_ini is None:
         for time in range(RandNum):
             x0 = np.random.uniform(0, 1, EqNum) * (dim_range[1] - dim_range[0]) + dim_range[0]
-            x, fval_dict, _, _ = sc.optimize.fsolve(func, x0, maxfev=450000, xtol=FixedPointConst, full_output=True)
+            x, fval_dict, _, _ = sc.optimize.fsolve(func_, x0, maxfev=450000, xtol=FixedPointConst, full_output=True)
             # fjac: the orthogonal matrix, q, produced by the QR factorization of the final approximate Jacobian matrix,
             # stored column wise; r: upper triangular matrix produced by QR factorization of the same matrix
             # if auto_func is None:
@@ -69,7 +82,7 @@ def gen_fixed_points(func, auto_func, dim_range, RandNum, EqNum, x_ini = None):
             #     jacobian_mat=(fval_dict["fjac"]).dot(matrixr)
             # else:
             fval = fval_dict['fvec']
-            jacobian_mat = nda.Jacobian(func)(np.array(x)) # autonp.array?
+            jacobian_mat = nda.Jacobian(func_)(np.array(x)) # autonp.array?
 
             jacobian_mat[np.isinf(jacobian_mat)] = 0
             if fval.dot(fval) < FixedPointConst:
@@ -84,7 +97,7 @@ def gen_fixed_points(func, auto_func, dim_range, RandNum, EqNum, x_ini = None):
     else:
         for time in range(x_ini.shape[0]):
             x0 = x_ini[time, :]
-            x, fval_dict, _, _ = sc.optimize.fsolve(func, x0, maxfev=450000, xtol=FixedPointConst, full_output=True)
+            x, fval_dict, _, _ = sc.optimize.fsolve(func_, x0, maxfev=450000, xtol=FixedPointConst, full_output=True)
 
             # if auto_func is None:
             #     fval, q, r = fval_dict['fvec'], fval_dict['fjac'], fval_dict['r']
@@ -93,7 +106,7 @@ def gen_fixed_points(func, auto_func, dim_range, RandNum, EqNum, x_ini = None):
             #     jacobian_mat=(fval_dict["fjac"]).dot(matrixr)
             # else:
             fval = fval_dict['fvec']
-            jacobian_mat = nda.Jacobian(func)(np.array(x)) # autonp.array?
+            jacobian_mat = nda.Jacobian(func_)(np.array(x)) # autonp.array?
 
             jacobian_mat[np.isfinite(jacobian_mat)] = 0
             if fval.dot(fval) < FixedPointConst:
@@ -105,6 +118,7 @@ def gen_fixed_points(func, auto_func, dim_range, RandNum, EqNum, x_ini = None):
                         break
                 if not Type[time]:
                     Type[time]=1
+
     for time in range(RandNum):
         if Type[time] == 0:
             continue