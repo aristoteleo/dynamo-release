bd9761a2c0cefadf342d9e9a788eea6cb6a8e3e8
Xiaojie Qiu <xqiu@tenzing.ucsf.edu>
2020-01-16 19:30:27 -0800

    streamline_plot

--- dynamo/plot/__init__.py
+++ dynamo/plot/__init__.py
@@ -5,7 +5,7 @@ from .theme import points
 from .preprocess import show_fraction, feature_genes, variance_explained
 from .scatters import scatters
 from .dynamics import phase_portraits, dynamics
-from .scVectorField import cell_wise_velocity, grid_velocity, stremline_plot, line_integral_conv # , plot_LIC_gray
+from .scVectorField import cell_wise_velocity, grid_velocity, streamline_plot, line_integral_conv # , plot_LIC_gray
 from .scPotential import show_landscape
 from .connectivity import nneighbors
 from .time_series import kinetic_heatmap, kinetic_curves
