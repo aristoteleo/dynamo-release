d88ecbe59a97dd37465e0f33f06a5bc6272fe86f
Xiaojie Qiu <xqiu@tenzing.ucsf.edu>
2020-01-27 15:52:38 -0800

    add consensus and hybrid -- jaccard + consensus in the velocity_metric file

--- dynamo/tools/velocity_metric.py
+++ dynamo/tools/velocity_metric.py
@@ -21,7 +21,8 @@ def cell_wise_confidence(adata, ekey='M_s', vkey='velocity_S', method='jaccard')
         vkey: 'str' (optional, default `velocity_S`)
             The dictionary key that corresponds to the estimated velocity values in layers attribute.
         method: `str` (optional, default `jaccard`)
-            Which method will be used for calculating the cell wise velocity confidence metric. By default it uses
+            Which method will be used for calculating the cell wise velocity confidence metric.
+            By default it uses
             `jaccard` index, which measures how well each velocity vector meets the geometric constraints defined by the
             local neighborhood structure. Jaccard index is calculated as the fraction of the number of the intersected
             set of nearest neighbors from each cell at current expression state (X) and that from the future expression
@@ -36,43 +37,44 @@ def cell_wise_confidence(adata, ekey='M_s', vkey='velocity_S', method='jaccard')
 
     X, V = (adata.X, adata.layers[vkey]) if ekey is 'X' else (adata.layers[ekey], adata.layers[vkey])
     n_neigh, X_neighbors = adata.uns['neighbors']['params']['n_neighbors'], adata.uns['neighbors']['connectivities']
+    n_pca_components = adata.obsm['X_pca'].shape[1]
 
     finite_inds = get_finite_inds(V, 0)
     X, V = X[:, finite_inds], V[:, finite_inds]
     if method == 'jaccard':
-        from sklearn.decomposition import TruncatedSVD
+        jac, _, _ = jaccard(X, V, n_pca_components, n_neigh, X_neighbors)
+        confidence = jac
 
-        n_pca_components = adata.obsm['X_pca'].shape[1]
-        transformer = TruncatedSVD(n_components=n_pca_components + 1, random_state=0)
-        Xt = X + V
-        if issparse(Xt):
-            Xt.data[Xt.data < 0] = 0
-            Xt.data = np.log2(Xt.data + 1)
-        else:
-            Xt = np.log2(Xt + 1)
-        X_fit = transformer.fit(Xt)
-        Xt_pca = X_fit.transform(Xt)[:, 1:]
+    elif method == 'hybrid':
+        # this is inspired from the localcity preservation paper
+        jac, intersect_, _ = jaccard(X, V, n_pca_components, n_neigh, X_neighbors)
 
-        V_neighbors, _, _, _ = umap_conn_indices_dist_embedding(Xt_pca, n_neighbors=n_neigh)
-        X_neighbors_, V_neighbors_ = X_neighbors.dot(X_neighbors), V_neighbors.dot(V_neighbors)
-        union_ = X_neighbors_ + V_neighbors_ > 0
-        intersect_ = mnn_from_list([X_neighbors_, V_neighbors_]) > 0
-
-        confidence = (intersect_.sum(1) / union_.sum(1)).A1 if issparse(X) else intersect_.sum(1) / union_.sum(1)
+        confidence = np.zeros(adata.n_obs)
+        for i in range(adata.n_obs):
+            neigh_ids = np.where(intersect_[i].A)[0]
+            confidence[i] = jac[i] * np.mean([consensus(V[i].A.flatten(), V[j].A.flatten()) for j in neigh_ids])  # X
 
     elif method == 'cosine':
         indices = adata.uns['neighbors']['indices']
         confidence = np.zeros(adata.n_obs)
         for i in range(adata.n_obs):
             neigh_ids = indices[i]
-            confidence[i] = np.mean([cosine(X[i].A.flatten(), V[j].A.flatten()) for j in neigh_ids])
+            confidence[i] = np.mean([cosine(V[i].A.flatten(), V[j].A.flatten()) for j in neigh_ids])  # X
 
-    elif method == 'correlation':
+    elif method == 'consensus':
         indices = adata.uns['neighbors']['indices']
         confidence = np.zeros(adata.n_obs)
         for i in range(adata.n_obs):
             neigh_ids = indices[i]
-            confidence[i] = np.mean([pearsonr(X[i].A.flatten(), V[j].A.flatten())[0] for j in neigh_ids])
+            confidence[i] = np.mean([consensus(V[i].A.flatten(), V[j].A.flatten()) for j in neigh_ids])  # X
+
+    elif method == 'correlation':
+        # this is equivalent to scVelo
+        indices = adata.uns['neighbors']['indices']
+        confidence = np.zeros(adata.n_obs)
+        for i in range(adata.n_obs):
+            neigh_ids = indices[i]
+            confidence[i] = np.mean([pearsonr(V[i].A.flatten(), V[j].A.flatten())[0] for j in neigh_ids]) # X
 
     elif method == 'divergence':
         pass
@@ -81,10 +83,37 @@ def cell_wise_confidence(adata, ekey='M_s', vkey='velocity_S', method='jaccard')
         raise Exception('The input {} method for cell-wise velocity confidence calculation is not implemented'
                         ' yet'.format(method))
 
-    adata.obs['confidence'] = confidence
+    adata.obs[method + '_velocity_confidence'] = confidence
 
     return adata
 
+def jaccard(X, V, n_pca_components, n_neigh, X_neighbors):
+    from sklearn.decomposition import TruncatedSVD
+
+    transformer = TruncatedSVD(n_components=n_pca_components + 1, random_state=0)
+    Xt = X + V
+    if issparse(Xt):
+        Xt.data[Xt.data < 0] = 0
+        Xt.data = np.log2(Xt.data + 1)
+    else:
+        Xt = np.log2(Xt + 1)
+    X_fit = transformer.fit(Xt)
+    Xt_pca = X_fit.transform(Xt)[:, 1:]
+
+    V_neighbors, _, _, _ = umap_conn_indices_dist_embedding(Xt_pca, n_neighbors=n_neigh)
+    X_neighbors_, V_neighbors_ = X_neighbors.dot(X_neighbors), V_neighbors.dot(V_neighbors)
+    union_ = X_neighbors_ + V_neighbors_ > 0
+    intersect_ = mnn_from_list([X_neighbors_, V_neighbors_]) > 0
+
+    jaccard = (intersect_.sum(1) / union_.sum(1)).A1 if issparse(X) else intersect_.sum(1) / union_.sum(1)
+
+    return jaccard, intersect_, union_
+
+def consensus(x, y):
+    x_norm, y_norm = np.linalg.norm(x), np.linalg.norm(y)
+    consensus = cosine(x, y) * np.min([x_norm, y_norm]) / np.max([x_norm, y_norm])
+
+    return consensus
 
 def curl(f,x):
     jac = nd.Jacobian(f)(x)
