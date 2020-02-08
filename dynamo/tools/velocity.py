b6f2ae54ec87efb164e7dd60c09819114197ffaf
Xiaojie Qiu <xqiu@tenzing.ucsf.edu>
2020-02-07 19:00:01 -0800

    use uncentered cov/var_x

--- dynamo/tools/velocity.py
+++ dynamo/tools/velocity.py
@@ -214,8 +214,13 @@ def fit_linreg(x, y, intercept=False):
         var_x = np.mean(xx * xx) - xm * xm
         k = cov / var_x
         b = ym - k * xm
+        # assume b is always positive
+        if b < 0: k, b = np.mean(xx * yy) / np.mean(xx * xx), 0
     else:
-        k = np.mean(yy) / np.mean(xx)
+        # use uncentered cov and var_x
+        cov = np.mean(xx * yy)
+        var_x = np.mean(xx * xx)
+        k = cov / var_x
         b = 0
 
     SS_tot_n = np.var(yy)
@@ -839,7 +844,7 @@ class estimation:
                           'delta_r2': None, "uu0": None, "ul0": None, "su0": None, "sl0": None, 'U0': None, 'S0': None, 'total0': None} # note that alpha_intercept also corresponds to u0 in fit_alpha_degradation, similar to fit_first_order_deg_lsq
         self.ind_for_proteins = ind_for_proteins
 
-    def fit(self, intercept=True, perc_left=5, perc_right=5, clusters=None):
+    def fit(self, intercept=False, perc_left=5, perc_right=5, clusters=None):
         """Fit the input data to estimate all or a subset of the parameters
 
         Arguments
