minor fix for Divergence calculation

dynamo/tools/vector_calculus.py
@@ -3,6 +3,7 @@ import numpy as np
 import numdifftools as nd
 from .scVectorField import vector_field_function
 
+
 def grad(f, x):
     """Gradient of scalar-valued function f evaluated at x"""
     return nd.Gradient(f)(x)
@@ -14,7 +15,7 @@ def laplacian(f, x):
     return sum(hes)
 
 
-def div(f, x):
+def divergence(f, x):
     """Divergence of the reconstructed vector field function f evaluated at x"""
     jac = nd.Jacobian(f)(x)
     return np.trace(jac)
@@ -44,23 +45,21 @@ def Curl(adata,
         adata: :class:`~anndata.AnnData`
             AnnData object that contains the reconstructed vector field function in the `uns` attribute.
         basis: `str` or None (default: `umap`)
-            The embedding data in which the vector field was reconstructed. If `basis` is either `umap` or `pca`, the
-            reconstructed trajector will be projected back to high dimensional space via the `inverse_transform` function.
+            The embedding data in which the vector field was reconstructed.
         vecfld_dict: `dict`
-            The true ODE function, useful when the data is generated through simulation. Replace VecFld arugment when
-            this has been set.
+            The true ODE function, useful when the data is generated through simulation.
 
     Returns
     -------
         adata: :class:`~anndata.AnnData`
-            AnnData object that is updated with the `Curl` key in the .obs.
+            AnnData object that is updated with the `curl` key in the .obs.
     """
 
     if vecfld_dict is None:
-        vf_key = 'VecFld' if basis == None else 'VecFld_' + basis
+        vf_key = 'VecFld' if basis is None else 'VecFld_' + basis
         if vf_key not in adata.uns.keys():
-            raise ValueError(f"Your adata doesn't have the key for Vector Field with {basis} basis."
-                             "Try firstly running dyn.tl.VectorField(adata, basis={basis}).")
+            raise ValueError(f"Your adata doesn't have the key for Vector Field with {basis} basis. "
+                             f"Try firstly running dyn.tl.VectorField(adata, basis={basis}).")
 
         vecfld_dict = adata.uns[vf_key]
 
@@ -69,7 +68,7 @@ def Curl(adata,
     curl = np.zeros((adata.n_obs, 1))
     func = lambda x: vector_field_function(x, vecfld_dict['VecFld'])
 
-    for i, x in tqdm(enumerate(X_data), "Calculating curl with the reconstructed vector field on the {basis} basis. "):
+    for i, x in tqdm(enumerate(X_data), f"Calculating curl with the reconstructed vector field on the {basis} basis. "):
         curl[i] = curl2d(func, x.flatten())
 
     adata.obs['curl'] = curl
@@ -86,23 +85,21 @@ def Divergence(adata,
         adata: :class:`~anndata.AnnData`
             AnnData object that contains the reconstructed vector field function in the `uns` attribute.
         basis: `str` or None (default: `umap`)
-            The embedding data in which the vector field was reconstructed. If `basis` is either `umap` or `pca`, the
-            reconstructed trajector will be projected back to high dimensional space via the `inverse_transform` function.
+            The embedding data in which the vector field was reconstructed.
         vecfld_dict: `dict`
-            The true ODE function, useful when the data is generated through simulation. Replace VecFld arugment when
-            this has been set.
+            The true ODE function, useful when the data is generated through simulation.
 
     Returns
     -------
         adata: :class:`~anndata.AnnData`
-            AnnData object that is updated with the `Curl` key in the .obs.
+            AnnData object that is updated with the `divergence` key in the .obs.
     """
 
     if vecfld_dict is None:
-        vf_key = 'VecFld' if basis == None else 'VecFld_' + basis
+        vf_key = 'VecFld' if basis is None else 'VecFld_' + basis
         if vf_key not in adata.uns.keys():
             raise ValueError(f"Your adata doesn't have the key for Vector Field with {basis} basis."
-                             "Try firstly running dyn.tl.VectorField(adata, basis={basis}).")
+                             f"Try firstly running dyn.tl.VectorField(adata, basis={basis}).")
 
         vecfld_dict = adata.uns[vf_key]
 
@@ -111,7 +108,7 @@ def Divergence(adata,
     div = np.zeros((adata.n_obs, 1))
     func = lambda x: vector_field_function(x, vecfld_dict['VecFld'])
 
-    for i, x in tqdm(enumerate(X_data), "Calculating curl with the reconstructed vector field on the {basis} basis. "):
-        curl[i] = div(func, x.flatten())
+    for i, x in tqdm(enumerate(X_data), f"Calculating divergence with the reconstructed vector field on the {basis} basis. "):
+        div[i] = divergence(func, x.flatten())
 
     adata.obs['divergence'] = div