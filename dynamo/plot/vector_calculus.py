fix curl calculation in 2D

dynamo/plot/vector_calculus.py
@@ -44,7 +44,7 @@ def curl(adata, color=None, cmap='bwr', *args, **kwargs):
     >>> dyn.tl.curl(adata)
     >>> dyn.pl.curl(adata)
 
-    See also:: :func:`../external.ddhodge.curl` for calculating curl with a diffusion graph built from reconstructed vector
+    See also:: :func:`..external.ddhodge.curl` for calculating curl with a diffusion graph built from reconstructed vector
     field.
     """
 
@@ -87,7 +87,7 @@ def divergence(adata, color=None, cmap='bwr', *args, **kwargs):
     >>> dyn.tl.divergence(adata)
     >>> dyn.pl.divergence(adata)
 
-    See also:: :func:`../external.ddhodge.divergence` for calculating divergence with a diffusion graph built from reconstructed
+    See also:: :func:`..external.ddhodge.divergence` for calculating divergence with a diffusion graph built from reconstructed
     vector field.
     """
 
@@ -95,6 +95,7 @@ def divergence(adata, color=None, cmap='bwr', *args, **kwargs):
     if not np.any(adata.obs.columns.isin(color_)):
         raise Exception(f"divergence is not existed in .obs, try run dyn.tl.divergence(adata) first.")
 
+    adata.obs.divergence = adata.obs.divergence.astype('float')
     adata_ = adata[~ adata.obs.divergence.isna(), :]
 
     if color is not None:
@@ -222,7 +223,7 @@ def jacobian(adata,
     gs = plt.GridSpec(nrow, ncol)
 
     for i, source in enumerate(source_gene):
-        for j, target in range(target_gene):
+        for j, target in enumerate(target_gene):
             ax = plt.subplot(gs[i * j + j])
             J = Der[i, j, :]
             cur_pd["jacobian"] = J

dynamo/tools/vector_calculus.py
@@ -73,7 +73,7 @@ def elementwise_jacobian_transformation(fjac, X, qi, qj):
 
     Js = fjac(X)
     ret = np.zeros(len(X))
-    for i in range(len(X)):
+    for i in tqdm(range(len(X)), "calculating Jacobian for each cell"):
         J = Js[:, :, i]
         ret[i] = qi @ J @ qj
     return ret
@@ -149,7 +149,7 @@ def _curl(f, x):
 def curl2d(f, x):
     """Curl of the reconstructed vector field f evaluated at x in 2D"""
     jac = nd.Jacobian(f)(x)
-    curl = jac[0, 0] - jac[1, 1] + jac[0, 1] - jac[1, 1]
+    curl = jac[1, 0] - jac[0, 1]
 
     return curl
 
@@ -353,8 +353,8 @@ def jacobian(adata,
         Jacobian = Jac_fun(X)
     else:
         if len(source_genes) == 1 and len(target_genes) == 1:
-            Jacobian = elementwise_jacobian_transformation(Jac_fun, X[cell_idx], Q[source_idx, :],
-                                                      Q[target_idx, :], timeit=True)
+            Jacobian = elementwise_jacobian_transformation(Jac_fun, X[cell_idx], Q[source_idx, :].flatten(),
+                                                      Q[target_idx, :].flatten(), timeit=True)
         else:
             Jacobian = subset_jacobian_transformation(Jac_fun, X[cell_idx], Q[source_idx, :],
                                                  Q[target_idx, :], timeit=True)