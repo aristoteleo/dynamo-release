048187193bf2586b60cfe6bd8371465534359a3b
Xiaojie Qiu <xqiu@uw.edu>
2020-08-17 17:34:51 -0400

    export est, mv, vf

--- dynamo/__init__.py
+++ dynamo/__init__.py
@@ -7,9 +7,12 @@ __version__ = get_version(__file__)
 del get_version
 
 from . import pp
+from . import est
 from . import tl
+from . import vf
 from . import pd
 from . import pl
+from . import mv
 from . import sim
 from .data_io import *
 from . import sample_data
