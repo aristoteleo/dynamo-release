287f6443a6a524030839d7c02e1b1c791a4d93c0
Xiaojie Qiu <xqiu@uw.edu>
2020-09-21 23:25:30 -0400

    update svg image links

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,9 +53,9 @@ nbsphinx_prolog = r"""
 
     <div class="note">
       <a href="https://colab.research.google.com/{{ docname|e }}" target="_parent">
-      <img src="https://github.com/aristoteleo/dynamo-release/raw/master/docs/source/_static/colab-badge.svg" alt="Open In Colab"/></a>
+      <img src="https://user-images.githubusercontent.com/7456281/93841442-99c3e180-fc61-11ea-9c87-07760b5dfc9a.png" alt="Open In Colab"/></a>
       <a href="https://nbviewer.jupyter.org/{{ docname|e }}" target="_parent">
-      <img src="https://github.com/aristoteleo/dynamo-release/raw/master/docs/source/_static/nbviewer-badge.svg" alt="Open In nbviewer"/></a>
+      <img src="https://user-images.githubusercontent.com/7456281/93841447-9c263b80-fc61-11ea-99b2-4eafe9958ee4.png" alt="Open In nbviewer"/></a>
     </div>
 """
 
