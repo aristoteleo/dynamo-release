73f226841addcfb959834df77de2c814b8d7c5a2
dummyindex <dummy_index@outlook.com>
2021-03-28 21:29:44 +0800

    fix a bug

--- dynamo/prediction/state_graph.py
+++ dynamo/prediction/state_graph.py
@@ -100,7 +100,7 @@ def state_graph(
             average=False,
             t_end=None,
         )
-        logger.report_progress(progress_name="KDTree parameter preparation computation")
+        logger.report_progress(percent=0, progress_name="KDTree parameter preparation computation")
         logger.log_time()
         kdt = cKDTree(all_X, leafsize=30)
         logger.finish_progress(progress_name="KDTree computation")
