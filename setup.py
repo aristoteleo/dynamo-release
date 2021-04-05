dae116f8ca4413b09017e75b152f47bd072e2a9f
dummyindex <dummy_index@outlook.com>
2021-04-05 13:46:27 +0800

    try fix networkx test install issue on github

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+
 # import numpy as np
 # from version import __version__
 from pathlib import Path
@@ -10,23 +11,22 @@ setup(
     name="dynamo-release",
     version="0.95.2",
     python_requires=">=3.7",
-    install_requires=[
-        l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
-    ], # 'yt>=3.5.1',
-    extras_require={"spatial": ["pysal>2.0.0"],
-                    "interactive_plots": ["plotly"],
-                    "network": ["networkx", "nxviz", "hiveplotlib"],
-                    "dimension_reduction": ["fitsne>=1.0.1", "dbmap>=1.1.2"],
-                    "test": ['sympy>=1.4'],
-                    "bigdata_visualization": ["datashader>=0.9.0", "bokeh>=1.4.0", "holoviews>=1.9.2"]
-                   },
+    install_requires=[l.strip() for l in Path("requirements.txt").read_text("utf-8").splitlines()],  # 'yt>=3.5.1',
+    extras_require={
+        "spatial": ["pysal>2.0.0"],
+        "interactive_plots": ["plotly"],
+        "network": ["networkx", "nxviz", "hiveplotlib"],
+        "dimension_reduction": ["fitsne>=1.0.1", "dbmap>=1.1.2"],
+        "test": ["sympy>=1.4", "networkx"],
+        "bigdata_visualization": ["datashader>=0.9.0", "bokeh>=1.4.0", "holoviews>=1.9.2"],
+    },
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-#     include_dirs=[np.get_include()],
+    #     include_dirs=[np.get_include()],
     author="Xiaojie Qiu, Yan Zhang",
     author_email="xqiu.sc@gmail.com",
     description="Mapping Vector Field of Single Cells",
