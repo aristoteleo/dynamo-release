c6369becffb8d399d46094c3bc06b6924a449f26
Xiaojie Qiu <xqiu@uw.edu>
2021-07-14 10:18:10 -0400

    fix typo succinct in tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,10 +8,10 @@ DEFAULT_MODE = "verbose"
 
 def test_config_change():
     assert dyn.config.DynamoAdataConfig.data_store_mode == "full"
-    dyn.config.DynamoAdataConfig.data_store_mode = "succint"
+    dyn.config.DynamoAdataConfig.data_store_mode = "succinct"
 
     # change class static values wont change config variables
-    assert dyn.config.DynamoAdataConfig.data_store_mode == "succint"
+    assert dyn.config.DynamoAdataConfig.data_store_mode == "succinct"
     assert dyn.config.DynamoAdataConfig.recipe_monocle_keep_filtered_cells_default == True
     assert dyn.config.DynamoAdataConfig.recipe_monocle_keep_filtered_cells_default == True
     assert dyn.config.DynamoAdataConfig.recipe_keep_raw_layers_default == False
@@ -19,7 +19,7 @@ def test_config_change():
     # update data store mode will update config variables as well
 
     # test succint mode
-    dyn.config.DynamoAdataConfig.update_data_store_mode("succint")
+    dyn.config.DynamoAdataConfig.update_data_store_mode("succinct")
     assert dyn.config.DynamoAdataConfig.recipe_keep_filtered_cells_default == False
     assert dyn.config.DynamoAdataConfig.recipe_keep_filtered_genes_default == False
     assert dyn.config.DynamoAdataConfig.recipe_keep_raw_layers_default == False
@@ -34,10 +34,10 @@ def test_config_change():
     assert dyn.config.DynamoAdataConfig.recipe_monocle_keep_filtered_cells_default == True
     assert dyn.config.DynamoAdataConfig.recipe_monocle_keep_filtered_cells_default == True
 
-    dyn.config.DynamoAdataConfig.update_data_store_mode("succint")
+    dyn.config.DynamoAdataConfig.update_data_store_mode("succinct")
     import dynamo.configuration as imported_config
 
-    assert imported_config.DynamoAdataConfig.data_store_mode == "succint"
+    assert imported_config.DynamoAdataConfig.data_store_mode == "succinct"
 
 
 if __name__ == "__main__":
