1f0f1210089762529092cd78198e53d58c01d7f0
user_name <12120524+dummyindex@users.noreply.github.com>
2022-02-03 06:27:57 -0500

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ from docs.source.utils import _download_notebook_dirs
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-source_suffix = [".rst", ".ipynb"]
+source_suffix = [".rst"]
 
 master_doc = "index"
 
