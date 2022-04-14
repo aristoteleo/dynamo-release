fb0c4a0928b400b76e783f5340dbbe1429692915
Xiaojie Qiu <xqiu@uw.edu>
2022-04-14 10:55:26 -0400

    fix get_version

--- dynamo/__init__.py
+++ dynamo/__init__.py
@@ -1,10 +1,10 @@
 """Mapping Vector Field of Single Cells
 """
 
-#from get_version import get_version
+from .get_version import get_version
 
-#__version__ = get_version(__file__)
-#del get_version
+__version__ = get_version(__file__)
+del get_version
 
 from . import pp
 from . import est
