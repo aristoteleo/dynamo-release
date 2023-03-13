0ecbd8424cad414878bcefef7bbbcac50f7238c1
Ke <dummtindex@gmail.com>
2023-03-13 11:16:18 -0400

    Update moments.py

--- dynamo/tools/moments.py
+++ dynamo/tools/moments.py
@@ -190,11 +190,11 @@ def moments(
     layers.sort(reverse=True)  # ensure we get M_us, M_tn, etc (instead of M_su or M_nt).
     for i, layer in enumerate(layers):
         layer_x = adata.layers[layer].copy()
-        matched_x_group = np.where([layer in x for x in [only_splicing, only_labeling, splicing_and_labeling]])
-        if len(matched_x_group[0]) == 0:
+        matched_x_group_indices = np.where([layer in x for x in [only_splicing, only_labeling, splicing_and_labeling]])
+        if len(matched_x_group_indices[0]) == 0:
             logger.warning(f"layer {layer} is not in any of the {only_splicing, only_labeling, splicing_and_labeling} groups, skipping...")
             continue
-        layer_x_group = matched_x_group[0][0]
+        layer_x_group = matched_x_group_indices[0][0]
         layer_x = inverse_norm(adata, layer_x)
 
         if mapper[layer] not in adata.layers.keys():
@@ -204,13 +204,13 @@ def moments(
                 else (conn.dot(layer_x), conn)
             )
         for layer2 in layers[i:]:
-            matched_group_indices = np.where([layer2 in x for x in [only_splicing, only_labeling, splicing_and_labeling]])
-            if len(matched_group_indices[0]) == 0:
+            matched_y_group_indices = np.where([layer2 in x for x in [only_splicing, only_labeling, splicing_and_labeling]])
+            if len(matched_y_group_indices[0]) == 0:
                 logger.warning(f"layer {layer2} is not in any of the {only_splicing, only_labeling, splicing_and_labeling} groups, skipping...")
                 continue
             layer_y = adata.layers[layer2].copy()
 
-            layer_y_group = matched_group_indices[0][0]
+            layer_y_group = matched_y_group_indices[0][0]
             # don't calculate 2 moments among uu, ul, su, sl -
             # they should be time-dependent moments and
             # those calculations are model specific
