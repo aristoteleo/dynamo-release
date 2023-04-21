Fix issues and reorder the normalization steps

dynamo/preprocessing/gene_selection.py
@@ -93,7 +93,7 @@ def Gini(adata: AnnData, layers: Union[Literal["all"], List[str]] = "all") -> An
 
 def select_genes_monocle(
     adata: AnnData,
-    layer: str = "X",
+    layer: str = DKM.X_LAYER,
     keep_filtered: bool = True,
     n_top_genes: int = 2000,
     sort_by: Literal["gini", "cv_dispersion", "fano_dispersion"] = "cv_dispersion",
@@ -139,13 +139,14 @@ def select_genes_monocle(
             feature_gene_idx = valid_table.index[feature_gene_idx]
             filter_bool = filter_bool.index.isin(feature_gene_idx)
         elif sort_by == "cv_dispersion" or sort_by == "fano_dispersion":
-            adata = select_genes_by_svr(
-                adata,
-                layers=[layer],
-                filter_bool=filter_bool,
-                algorithm=sort_by,
-                **SVRs_kwargs,
-            )
+            if not any("velocyto_SVR" in key for key in adata.uns.keys()):
+                select_genes_by_svr(
+                    adata,
+                    layers=layer,
+                    filter_bool=filter_bool,
+                    algorithm=sort_by,
+                    **SVRs_kwargs,
+                )
             filter_bool = get_svr_filter(adata, layer=layer, n_top_genes=n_top_genes, return_adata=False)
         else:
             raise NotImplementedError(f"The algorithm {sort_by} is invalid/unsupported")
@@ -153,6 +154,7 @@ def select_genes_monocle(
     # filter genes by gene expression fraction as well
     if "frac" not in adata.var.keys():
         adata.var["frac"], invalid_ids = compute_gene_exp_fraction(X=adata.X, threshold=exprs_frac_for_gene_exclusion)
+
     genes_to_exclude = (
         list(adata.var_names[invalid_ids])
         if genes_to_exclude is None
@@ -229,7 +231,7 @@ def select_genes_by_svr(
             continue
 
         mean, cv = get_mean_cv(valid_CM, algorithm, winsorize, winsor_perc)
-        fitted_fun = get_prediction_by_svr(mean, cv, svr_gamma)
+        fitted_fun, svr_gamma = get_prediction_by_svr(mean, cv, svr_gamma)
         score = cv - fitted_fun(mean)
         if sort_inverse:
             score = -score
@@ -317,13 +319,13 @@ def get_vaild_CM(
     if total_szfactor is not None and total_szfactor in adata.obs.keys():
         szfactors = adata.obs[total_szfactor].values[:, None] if total_szfactor in adata.obs.columns else None
 
-    if szfactors is not None and relative_expr:
-        if issparse(CM):
-            from sklearn.utils import sparsefuncs
-
-            sparsefuncs.inplace_row_scale(CM, 1 / szfactors)
-        else:
-            CM /= szfactors
+    # if szfactors is not None and relative_expr:
+    #     if issparse(CM):
+    #         from sklearn.utils import sparsefuncs
+    #
+    #         sparsefuncs.inplace_row_scale(CM, 1 / szfactors)
+    #     else:
+    #         CM /= szfactors
 
     if winsorize:
         if min_expr_cells <= ((100 - winsor_perc[1]) * CM.shape[0] * 0.01):
@@ -359,7 +361,6 @@ def get_mean_cv(
         (gene_counts_stats, gene_fano_parameters) = get_highvar_genes_sparse(valid_CM)
         mean = np.array(gene_counts_stats["mean"]).flatten()[:, None]
         cv = np.array(gene_counts_stats["fano"]).flatten()
-        mu = gene_counts_stats["mean"]
         return mean, cv
     elif algorithm == "cv_dispersion":
         if winsorize:
@@ -418,7 +419,7 @@ def get_prediction_by_svr(ground: np.ndarray, target: np.ndarray, svr_gamma: Opt
     # Fit the Support Vector Regression
     clf = SVR(gamma=svr_gamma)
     clf.fit(ground, target)
-    return clf.predict
+    return clf.predict, svr_gamma
 
 
 # Highly variable gene selection function:

dynamo/preprocessing/preprocessor_utils.py
@@ -66,7 +66,7 @@ def is_log1p_transformed_adata(adata: anndata.AnnData) -> bool:
     chosen_gene_indices = np.random.choice(adata.n_vars, 10)
     _has_log1p_transformed = not np.allclose(
         np.array(adata.X[:, chosen_gene_indices].sum(1)),
-        np.array(adata.layers["spliced"][:, chosen_gene_indices].sum(1)),
+        np.array(adata.layers["X_spliced"][:, chosen_gene_indices].sum(1)),
         atol=1e-4,
     )
     return _has_log1p_transformed
@@ -793,10 +793,8 @@ def normalize_cell_expr_by_size_factors(
     layer_sz_column_names.extend(["Size_Factor"])
     # layers_to_sz = list(set(layer_sz_column_names).difference(adata.obs.keys()))
     layers_to_sz = list(set(layer_sz_column_names))
-    if len(layers_to_sz) > 0:
-        layers = pd.Series(layers_to_sz).str.split("_Size_Factor", expand=True).iloc[:, 0].tolist()
-        if "Size_Factor" in layers:
-            layers[np.where(np.array(layers) == "Size_Factor")[0][0]] = "X"
+
+    if not all(key in adata.obs.keys() for key in layers_to_sz):
         calc_sz_factor(
             adata,
             layers=layers,
@@ -806,13 +804,16 @@ def normalize_cell_expr_by_size_factors(
             scale_to=scale_to,
         )
 
+    layers = pd.Series(layers_to_sz).str.split("_Size_Factor", expand=True).iloc[:, 0].tolist()
+    layers[np.where(np.array(layers) == "Size_Factor")[0][0]] = "X"
+
     excluded_layers = []
     if not X_total_layers:
         excluded_layers.extend(["X"])
     if not splicing_total_layers:
         excluded_layers.extend(["spliced", "unspliced"])
 
-    main_info("size factor normalize following layers: " + str(layers))
+    main_debug("size factor normalize following layers: " + str(layers))
     for layer in layers:
         if layer in excluded_layers:
             szfactors, CM = get_sz_exprs(adata, layer, total_szfactor=None)
@@ -859,7 +860,7 @@ def normalize_cell_expr_by_size_factors(
             main_warning(_norm_method + " is not implemented yet")
 
         if layer in ["raw", "X"]:
-            main_info("set adata <X> to normalized data using %s" % _norm_method)
+            main_debug("set adata <X> to normalized data using %s" % _norm_method)
             adata.X = CM
         elif layer == "protein" and "protein" in adata.obsm_keys():
             main_info_insert_adata_obsm("X_protein")