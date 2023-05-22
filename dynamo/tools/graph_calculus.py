graph calculus jacobian fix

dynamo/tools/graph_calculus.py
@@ -238,7 +238,7 @@ def graphize_velocity_coopt(
 
             # cosine similarity
             w_norm = np.linalg.norm(w)
-            if w_norm == 0 or b == 0:
+            if w_norm == 0 or b == 0 or c_norm==0:
                 jac_sim = 0
             else:
                 jac_sim = b * (c / (w_norm * c_norm) - w.dot(c) / (w_norm**3 * c_norm) * w)
@@ -361,7 +361,7 @@ def _graphize_velocity_coopt(
                 jac_con = jac_con / (v_ - v).dot(v_ - v)
 
             # cosine similarity
-            if w_norm == 0 or b == 0 or c_norm==0:
+            if v_norm == 0 or b == 0:
                 jac_sim = 0
             else:
                 jac_sim = b / v_norm**2 * (v_norm * D @ u_ - v_.dot(u_) * v_ @ D.T / v_norm)