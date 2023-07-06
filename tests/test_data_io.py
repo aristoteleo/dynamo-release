a7738cd969f874ca8daed7157c736fb485c6c4dc
sichao <sichao_y@outlook.com>
2023-07-06 15:53:26 -0400

    further save memory

--- tests/test_data_io.py
+++ tests/test_data_io.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import dynamo
 import dynamo as dyn
-
+import pytest
 # import utils
 
 
@@ -12,18 +12,14 @@ def test_save_rank_info(processed_zebra_adata):
     dyn.export_rank_xlsx(processed_zebra_adata)
 
 
+@pytest.mark.skip(reason="excessive memory usage")
 def test_scEU_seq():
     dynamo.sample_data.scEU_seq_rpe1()
     assert os.path.exists("./data/rpe1.h5ad")
 
 
-def test_zebrafish():
-    dynamo.sample_data.zebrafish()
-
-
 if __name__ == "__main__":
     # test_scEU_seq()
-    # test_zebrafish()
     # adata = utils.gen_or_read_zebrafish_data()
     # test_save_rank_info(adata)
     pass
