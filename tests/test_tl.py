d43915e4f4e04933aee3f35b32aadd84344bbad8
Sichao25 <sichao_y@outlook.com>
2023-07-27 19:12:50 -0400

    create tests for 1nd and 2nd moments

--- tests/test_tl.py
+++ tests/test_tl.py
@@ -4,6 +4,33 @@ import pytest
 import dynamo as dyn
 
 
+def test_calc_1nd_moment():
+    X = np.array([[1, 2], [3, 4], [5, 6]])
+    W = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
+    result = dyn.tl.calc_1nd_moment(X, W, normalize_W=False)
+    expected_result = np.array([[3, 4], [6, 8], [3, 4]])
+    assert np.array_equal(result, expected_result)
+
+    result, normalized_W = dyn.tl.calc_1nd_moment(X, W, normalize_W=True)
+    expected_result = np.array([[3.0, 4.0], [3.0, 4.0], [3.0, 4.0]])
+    assert np.array_equal(result, expected_result)
+    assert np.array_equal(normalized_W,
+                          np.array([[0.0, 1, 0.0], [0.5, 0.0, 0.5], [0.0, 1, 0.0]]))
+
+
+def test_calc_2nd_moment():
+    X = np.array([[1, 2], [3, 4], [5, 6]])
+    Y = np.array([[2, 3], [4, 5], [6, 7]])
+    W = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
+    result = dyn.tl.calc_2nd_moment(X, Y, W, normalize_W=False, center=False)
+    expected_result = np.array([[12, 20], [32, 48], [12, 20]])
+    assert np.array_equal(result, expected_result)
+
+    result = dyn.tl.calc_2nd_moment(X, Y, W, normalize_W=True, center=False)
+    expected_result = np.array([[12., 20.], [16., 24.], [12., 20.]])
+    assert np.array_equal(result, expected_result)
+
+
 def test_sampling():
     arr = np.random.rand(20, 3)
     n = 2
