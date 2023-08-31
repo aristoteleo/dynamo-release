194c0b885368524c8ac7f40ab28ce9862f546102
sichao <sichao_y@outlook.com>
2023-08-31 16:08:05 -0400

    add 3D option to remove_particles

--- dynamo/movie/utils.py
+++ dynamo/movie/utils.py
@@ -1,10 +1,18 @@
-from typing import Union
+from typing import Optional, Union
+
+import numpy as np
 
 
-def remove_particles(pts: list, xlim: Union[tuple, list], ylim: Union[tuple, list]):
+def remove_particles(
+    pts: list,
+    xlim: Union[tuple, list],
+    ylim: Union[tuple, list],
+    zlim: Optional[Union[tuple, list]] = None,
+):
     if len(pts) == 0:
         return []
     outside_xlim = (pts[:, 0] < xlim[0]) | (pts[:, 0] > xlim[1])
     outside_ylim = (pts[:, 1] < ylim[0]) | (pts[:, 1] > ylim[1])
-    keep = ~(outside_xlim | outside_ylim)
+    outside_zlim = np.full(outside_xlim.shape, False) if zlim is None else (pts[:, 2] < ylim[0]) | (pts[:, 2] > ylim[1])
+    keep = ~(outside_xlim | outside_ylim | outside_zlim)
     return pts[keep]
