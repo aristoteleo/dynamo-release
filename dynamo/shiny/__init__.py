33c99aee52e804091b043f7dcd932eea9b434595
sichao <sichao_y@outlook.com>
2023-09-11 16:24:18 -0400

    initial commit for lap shiny app

--- dynamo/shiny/__init__.py
+++ dynamo/shiny/__init__.py
@@ -1,1 +1,2 @@
+from .lap import lap_web_app
 from .perturbation import perturbation_web_app
---
+++ dynamo/shiny/lap.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import shiny.experimental as x
+from shiny import App, reactive, render, ui
+
+from ..plot import streamline_plot
+
+
+def lap_web_app(input_adata):
+    app_ui = x.ui.page_sidebar(
+        x.ui.sidebar(
+            x.ui.accordion(
+                x.ui.accordion_panel(
+                    "Streamline Plot",
+                    ui.input_text("color", "color", placeholder="Enter color"),
+                    ui.input_text("streamline_basis", "output basis", placeholder="Enter basis"),
+                ),
+            ),
+        ),
+        ui.div(
+            ui.input_action_button(
+                "activate_streamline_plot", "Run streamline plot", class_="btn-primary"
+            )
+        ),
+        x.ui.output_plot("base_streamline_plot"),
+    )
+
+    def server(input, output, session):
+        adata = input_adata.copy()
+
+        @output
+        @render.plot()
+        @reactive.event(input.activate_streamline_plot)
+        def base_streamline_plot():
+            color = input.color().split(",")
+
+            axes_list = streamline_plot(adata, color=color, basis=input.streamline_basis(), save_show_or_return="return")
+
+            # remove colorbar from the fig because shiny is incompatible matplotlib>=3.7
+            fig = plt.gcf()
+            for ax in fig.get_axes():
+                if ax.get_subplotspec() is None:
+                    ax.remove()
+
+            return fig
+
+    app = App(app_ui, server, debug=True)
+    app.run()
