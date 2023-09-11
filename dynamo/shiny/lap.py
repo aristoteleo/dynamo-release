e5065a6f5c44a4741ab64442ddccb9c3d2d6960c
sichao <sichao_y@outlook.com>
2023-09-11 17:51:36 -0400

    implement lap initialization

--- dynamo/shiny/lap.py
+++ dynamo/shiny/lap.py
@@ -5,7 +5,8 @@ from shiny import App, reactive, render, ui
 
 from .utils import filter_fig
 from ..plot import streamline_plot
-from ..tools import select_cell
+from ..tools import neighbors
+from ..tools.utils import nearest_neighbors, select_cell
 
 
 def lap_web_app(input_adata):
@@ -22,6 +23,11 @@ def lap_web_app(input_adata):
                 x.ui.accordion_panel(
                     "Initialization",
                     ui.input_text("cells_names", "cells names", placeholder="Enter names of cell"),
+                    ui.input_text(
+                        "fps_coordinates",
+                        "fixed points coordinates",
+                        placeholder="Enter the coordinates of fixed point."
+                    ),
                 ),
             ),
         ),
@@ -42,6 +48,7 @@ def lap_web_app(input_adata):
     def server(input, output, session):
         adata = input_adata.copy()
         cells = reactive.Value[list[np.ndarray]]()
+        cells_indices = reactive.Value[list[list[float]]]()
 
         @output
         @render.plot()
@@ -52,13 +59,24 @@ def lap_web_app(input_adata):
             for c in cells_names:
                 cell_list.append(select_cell(adata, input.cells_type_key(), c))
 
+            fps_coordinates = [float(num) for num in input.fps_coordinates().split(",")]
+            fps_coordinates = [[fps_coordinates[i], fps_coordinates[i + 1]] for i in range(0, len(fps_coordinates), 2)]
+
+            cells_indices_list = []
+            for coord in fps_coordinates:
+                cells_indices_list.append(nearest_neighbors(coord, adata.obsm["X_" + input.streamline_basis()]))
+
             cells.set(cell_list)
             plt.scatter(*adata.obsm["X_" + input.streamline_basis()].T)
+            for indices in cells_indices_list:
+                plt.scatter(*adata[indices[0]].obsm["X_" + input.streamline_basis()].T)
 
         @output
         @render.plot()
         @reactive.event(input.activate_streamline_plot)
         def base_streamline_plot():
+            neighbors(adata, basis=input.streamline_basis(), result_prefix=input.streamline_basis())
+
             color = input.cells_type_key().split(",")
 
             axes_list = streamline_plot(adata, color=color, basis=input.streamline_basis(), save_show_or_return="return")
