c9b9eed1dc09550f52a5157c1648aed3bb3fec91
sichao <sichao_y@outlook.com>
2023-09-19 11:40:34 -0400

    add styles to perturbation

--- dynamo/shiny/perturbation.py
+++ dynamo/shiny/perturbation.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import shiny.experimental as x
+from htmltools import div
+from pathlib import Path
 from shiny import App, reactive, render, ui
 
 from .utils import filter_fig
@@ -7,12 +9,16 @@ from ..plot import streamline_plot
 from ..prediction import perturbation
 
 
+css_path = Path(__file__).parent / "styles.css"
+
+
 def perturbation_web_app(input_adata):
     app_ui = x.ui.page_sidebar(
         x.ui.sidebar(
+            ui.include_css(css_path),
             x.ui.accordion(
                 x.ui.accordion_panel(
-                    "Perturbation",
+                    div("Perturbation", class_="bold-subtitle"),
                     ui.input_text("selected_genes", "Genes to perform perturbation: ", placeholder="e.g. GATA1"),
                     ui.input_text("emb_basis", "Basis from which the vector field is reconstructed: ", value="umap"),
                     ui.input_text(
@@ -20,23 +26,26 @@ def perturbation_web_app(input_adata):
                     ),
                     ui.input_action_button(
                         "activate_purterbation", "Run perturbation", class_="btn-primary"
-                    )
+                    ),
+                    value="Perturbation",
                 ),
                 x.ui.accordion_panel(
-                    "Streamline Plot",
+                    div("Streamline Plot", class_="bold-subtitle"),
                     ui.input_text("color", "The key to color the cells: ", value="cell_type"),
                     ui.input_text(
                         "streamline_basis", "The perturbation output as the basis of plot: ", value="umap_perturbation"
                     ),
                     ui.input_action_button(
                         "activate_streamline_plot", "Streamline plot", class_="btn-primary"
-                    )
+                    ),
+                    value="Streamline Plot",
                 ),
                 open=False,
             ),
             width=500,
         ),
         ui.div(
+            div("Streamline Plot", class_="bold-subtitle"),
             x.ui.output_plot("perturbation_plot"),
         ),
     )
