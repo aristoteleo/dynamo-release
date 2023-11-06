3a5d71481d4acc57126fafc78c03d9a95154fb1b
sichao <sichao_y@outlook.com>
2023-11-06 15:53:25 -0500

    debug show_landscape

--- dynamo/plot/scPotential.py
+++ dynamo/plot/scPotential.py
@@ -15,9 +15,9 @@ from .utils import save_fig
 
 def show_landscape(
     adata: AnnData,
-    Xgrid: np.ndarray,
-    Ygrid: np.ndarray,
-    Zgrid: np.ndarray,
+    Xgrid: Optional[np.ndarray] = None,
+    Ygrid: Optional[np.ndarray] = None,
+    Zgrid: Optional[np.ndarray] = None,
     basis: str = "umap",
     save_show_or_return: Literal["save", "show", "return"] = "show",
     save_kwargs: Dict[str, Any] = {},
@@ -49,9 +49,9 @@ def show_landscape(
             adata.uns["grid_Pot_" + basis]["Zgrid"],
         )
 
-    Xgrid = Xgrid_ if Xgrid is None else Xgrid
-    Ygrid = Ygrid_ if Ygrid is None else Ygrid
-    Zgrid = Zgrid_ if Zgrid is None else Zgrid
+    Xgrid = np.nan_to_num(Xgrid_) if Xgrid is None else np.nan_to_num(Xgrid)
+    Ygrid = np.nan_to_num(Ygrid_) if Ygrid is None else np.nan_to_num(Ygrid)
+    Zgrid = np.nan_to_num(Zgrid_) if Zgrid is None else np.nan_to_num(Zgrid)
 
     import matplotlib.pyplot as plt
     from matplotlib import cm
@@ -60,7 +60,7 @@ def show_landscape(
     from mpl_toolkits.mplot3d import Axes3D
 
     fig = plt.figure()
-    ax = fig.gca(projection="3d")
+    ax = fig.add_subplot(projection="3d")
 
     # Plot the surface.
     ls = LightSource(azdeg=0, altdeg=65)
