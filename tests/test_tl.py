c535d94dcde1c73d460e240c1d7e8c7f2b696fa3
Sichao25 <sichao_y@outlook.com>
2023-11-10 18:48:29 -0500

    merge clustering tests to tools tests

--- tests/test_tl.py
+++ tests/test_tl.py
@@ -99,3 +99,52 @@ def test_sampling():
 def test_score_cells(adata):
     scores = dyn.tl.score_cells(adata)
     assert scores.shape[0] == adata.n_obs
+
+
+# clustering
+@pytest.mark.skip(reason="dependency not installed")
+def test_simple_cluster_community_adata(adata):
+    adata = adata.copy()
+    dyn.tl.louvain(adata)
+    dyn.tl.leiden(adata)
+
+    try:
+        dyn.tl.louvain(adata, directed=True)
+    except ValueError as e:
+        print("################################################")
+        print("PASSED: Value error captured as EXPECTED, Exception info:")
+        print(e)
+        print("################################################")
+
+    dyn.tl.leiden(adata, directed=True)
+    assert np.all(adata.obs["louvain"] != -1)
+    assert np.all(adata.obs["leiden"] != -1)
+
+    # dyn.pl.louvain(adata, basis="pca")
+    # dyn.pl.leiden(adata, basis="pca")
+    # dyn.pl.infomap(adata, basis="pca")
+
+
+@pytest.mark.skip(reason="umap compatability issue with numpy, pynndescent and pytest")
+def test_simple_cluster_field(adata):
+    adata = adata.copy()
+    dyn.tl.reduceDimension(adata, basis="umap", n_pca_components=30, enforce=True)
+    dyn.tl.cell_velocities(adata, basis="umap")
+    dyn.vf.VectorField(adata, basis="umap", M=100)
+    dyn.vf.cluster_field(adata, basis="umap", method="louvain")
+    dyn.vf.cluster_field(adata, basis="umap", method="leiden")
+    assert np.all(adata.obs["louvain"] != -1)
+    assert np.all(adata.obs["leiden"] != -1)
+
+
+@pytest.mark.skip(reason="dependency not installed")
+def test_leiden_membership_input(adata):
+    adata = adata.copy()
+    # somehow this initial membership works before, but not now
+    initial_membership = np.random.randint(low=0, high=min(100, len(adata)), size=len(adata), dtype=int)
+    dyn.tl.leiden(adata, initial_membership=initial_membership)
+    assert np.all(adata.obs["leiden"] != -1)
+
+    initial_membership = np.random.randint(low=0, high=100, size=len(adata), dtype=int)
+    dyn.tl.leiden(adata, directed=True, initial_membership=initial_membership)
+    assert np.all(adata.obs["leiden"] != -1)
