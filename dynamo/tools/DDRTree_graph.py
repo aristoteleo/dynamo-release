01741e39d6cbae1f14e3b7dc2a6e9d0a2c35e412
sichao <sichao_y@outlook.com>
2023-11-21 14:32:24 -0500

    reorganize orders in files

--- dynamo/tools/DDRTree_graph.py
+++ dynamo/tools/DDRTree_graph.py
@@ -10,6 +10,138 @@ from .DDRTree import cal_ncenter, DDRTree
 from ..dynamo_logger import main_info, main_info_insert_adata_uns
 
 
+def construct_velocity_tree(adata: AnnData, transition_matrix_key: str = "pearson"):
+    """Integrate pseudotime ordering with velocity to automatically assign the direction of the learned trajectory.
+
+    Args:
+        adata: The anndata object containing the single-cell data.
+        transition_matrix_key (str, optional): Key to the transition matrix in the `adata.obsp` object that represents
+            the transition probabilities between cells. Defaults to "pearson".
+
+    Raises:
+        KeyError: If the transition matrix or cell order information is not found in the `adata` object.
+
+    Returns:
+        A directed velocity tree represented as a NumPy array.
+    """
+    if transition_matrix_key + "_transition_matrix" not in adata.obsp.keys():
+        raise KeyError("Transition matrix not found in anndata. Please call cell_velocities() before constructing "
+                       "velocity tree")
+
+    if "cell_order" not in adata.uns.keys():
+        raise KeyError("Cell order information not found in anndata. Please call order_cells() before constructing "
+                       "velocity tree.")
+
+    main_info("Constructing velocity tree...")
+
+    transition_matrix = adata.obsp[transition_matrix_key + "_transition_matrix"]
+    R = adata.uns["cell_order"]["R"]
+    orders = np.argsort(adata.uns["cell_order"]["centers_order"])
+    parents = [adata.uns["cell_order"]["centers_parent"][node] for node in orders]
+    velocity_tree = adata.uns["cell_order"]["centers_minSpanningTree"]
+    directed_velocity_tree = velocity_tree.copy()
+
+    segments = _get_all_segments(orders, parents)
+    center_transition_matrix = _compute_center_transition_matrix(transition_matrix, R)
+
+    for segment in segments:
+        edge_pairs = _get_edges(segment)
+        edge_pairs_reversed = _get_edges(segment[::-1])
+        segment_p = _calculate_segment_probability(center_transition_matrix, edge_pairs)
+        segment_p_reveresed = _calculate_segment_probability(center_transition_matrix, edge_pairs_reversed)
+        if segment_p[-1] > segment_p_reveresed[-1]:
+            for i, (r, c) in enumerate(edge_pairs):
+                directed_velocity_tree[r, c] = max(velocity_tree[r, c], velocity_tree[c, r])
+                directed_velocity_tree[c, r] = 0
+        elif segment_p[-1] < segment_p_reveresed[-1]:
+            for i, (r, c) in enumerate(edge_pairs):
+                directed_velocity_tree[c, r] = max(velocity_tree[r, c], velocity_tree[c, r])
+                directed_velocity_tree[r, c] = 0
+        else:
+            for i, (r, c) in enumerate(edge_pairs):
+                directed_velocity_tree[c, r] = velocity_tree[c, r]
+                directed_velocity_tree[r, c] = velocity_tree[r, c]
+
+    adata.uns["directed_velocity_tree"] = directed_velocity_tree
+    main_info_insert_adata_uns("directed_velocity_tree")
+    return directed_velocity_tree
+
+
+def directed_pg(
+    adata: AnnData,
+    basis: str = "umap",
+    transition_method: str = "pearson",
+    maxIter: int = 10,
+    sigma: float = 0.001,
+    Lambda: Optional[float] = None,
+    gamma: float = 10,
+    ncenter: Optional[int] = None,
+    raw_embedding: bool = True,
+) -> AnnData:
+    """A function that learns a direct principal graph by integrating the transition matrix between and DDRTree.
+
+    Args:
+        adata: An AnnData object,
+        basis: The dimension reduction method utilized. Defaults to "umap".
+        transition_method: The method to calculate the transition matrix and project high dimensional vector to low
+            dimension.
+        maxIter: The max iteration numbers. Defaults to 10.
+        sigma: The bandwidth parameter. Defaults to 0.001.
+        Lambda: The regularization parameter for inverse graph embedding. Defaults to None.
+        gamma: The regularization parameter for k-means. Defaults to 10.
+        ncenter: The number of centers to be considered. If None, number of centers would be calculated automatically.
+            Defaults to None.
+        raw_embedding: Whether to project the nodes on the principal graph into the original embedding. Defaults to
+            True.
+
+    Raises:
+        Exception: invalid `basis`.
+        Exception: adata.uns["transition_matrix"] not defined.
+
+    Returns:
+        An updated AnnData object that is updated with principal_g_transition, X__DDRTree and X_DDRTree_pg keys.
+    """
+
+    X = adata.obsm["X_" + basis] if "X_" + basis in adata.obsm.keys() else None
+    if X is None:
+        raise Exception("{} is not a key of obsm ({} dimension reduction is not performed yet.).".format(basis, basis))
+
+    transition_matrix = (
+        adata.obsp[transition_method + "_transition_matrix"]
+        if transition_method + "_transition_matrix" in adata.obsp.keys()
+        else None
+    )
+    if transition_matrix is None:
+        raise Exception("transition_matrix is not a key of uns. Please first run cell_velocity.")
+
+    Lambda = 5 * X.shape[1] if Lambda is None else Lambda
+    ncenter = 250 if cal_ncenter(X.shape[1]) is None else ncenter
+
+    Z, Y, principal_g, cell_membership, W, Q, C, objs = DDRTree(
+        X,
+        maxIter=maxIter,
+        Lambda=Lambda,
+        sigma=sigma,
+        gamma=gamma,
+        ncenter=ncenter,
+    )
+
+    X = csr_matrix(principal_g)
+    Tcsr = minimum_spanning_tree(X)
+    principal_g = Tcsr.toarray().astype(int)
+
+    # here we can also identify siginificant links using methods related to PAGA
+    transition_matrix = transition_matrix.toarray()
+    principal_g_transition = cell_membership.T.dot(transition_matrix).dot(cell_membership) * principal_g
+
+    adata.uns["principal_g_transition"] = principal_g_transition
+    adata.obsm["X_DDRTree"] = X.T if raw_embedding else Z
+    cell_membership = csr_matrix(cell_membership)
+    adata.uns["X_DDRTree_pg"] = cell_membership.dot(X.T) if raw_embedding else Y
+
+    return adata
+
+
 def _compute_center_transition_matrix(transition_matrix: Union[csr_matrix, np.ndarray], R: np.ndarray) -> np.ndarray:
     """Calculate the transition matrix for DDRTree centers.
 
@@ -160,135 +292,3 @@ def _get_all_segments(orders: Union[np.ndarray, List], parents: Union[np.ndarray
             segments.append(path)
 
     return segments
-
-
-def construct_velocity_tree(adata: AnnData, transition_matrix_key: str = "pearson"):
-    """Integrate pseudotime ordering with velocity to automatically assign the direction of the learned trajectory.
-
-    Args:
-        adata: The anndata object containing the single-cell data.
-        transition_matrix_key (str, optional): Key to the transition matrix in the `adata.obsp` object that represents
-            the transition probabilities between cells. Defaults to "pearson".
-
-    Raises:
-        KeyError: If the transition matrix or cell order information is not found in the `adata` object.
-
-    Returns:
-        A directed velocity tree represented as a NumPy array.
-    """
-    if transition_matrix_key + "_transition_matrix" not in adata.obsp.keys():
-        raise KeyError("Transition matrix not found in anndata. Please call cell_velocities() before constructing "
-                       "velocity tree")
-
-    if "cell_order" not in adata.uns.keys():
-        raise KeyError("Cell order information not found in anndata. Please call order_cells() before constructing "
-                       "velocity tree.")
-
-    main_info("Constructing velocity tree...")
-
-    transition_matrix = adata.obsp[transition_matrix_key + "_transition_matrix"]
-    R = adata.uns["cell_order"]["R"]
-    orders = np.argsort(adata.uns["cell_order"]["centers_order"])
-    parents = [adata.uns["cell_order"]["centers_parent"][node] for node in orders]
-    velocity_tree = adata.uns["cell_order"]["centers_minSpanningTree"]
-    directed_velocity_tree = velocity_tree.copy()
-
-    segments = _get_all_segments(orders, parents)
-    center_transition_matrix = _compute_center_transition_matrix(transition_matrix, R)
-
-    for segment in segments:
-        edge_pairs = _get_edges(segment)
-        edge_pairs_reversed = _get_edges(segment[::-1])
-        segment_p = _calculate_segment_probability(center_transition_matrix, edge_pairs)
-        segment_p_reveresed = _calculate_segment_probability(center_transition_matrix, edge_pairs_reversed)
-        if segment_p[-1] > segment_p_reveresed[-1]:
-            for i, (r, c) in enumerate(edge_pairs):
-                directed_velocity_tree[r, c] = max(velocity_tree[r, c], velocity_tree[c, r])
-                directed_velocity_tree[c, r] = 0
-        elif segment_p[-1] < segment_p_reveresed[-1]:
-            for i, (r, c) in enumerate(edge_pairs):
-                directed_velocity_tree[c, r] = max(velocity_tree[r, c], velocity_tree[c, r])
-                directed_velocity_tree[r, c] = 0
-        else:
-            for i, (r, c) in enumerate(edge_pairs):
-                directed_velocity_tree[c, r] = velocity_tree[c, r]
-                directed_velocity_tree[r, c] = velocity_tree[r, c]
-
-    adata.uns["directed_velocity_tree"] = directed_velocity_tree
-    main_info_insert_adata_uns("directed_velocity_tree")
-    return directed_velocity_tree
-
-
-def directed_pg(
-    adata: AnnData,
-    basis: str = "umap",
-    transition_method: str = "pearson",
-    maxIter: int = 10,
-    sigma: float = 0.001,
-    Lambda: Optional[float] = None,
-    gamma: float = 10,
-    ncenter: Optional[int] = None,
-    raw_embedding: bool = True,
-) -> AnnData:
-    """A function that learns a direct principal graph by integrating the transition matrix between and DDRTree.
-
-    Args:
-        adata: An AnnData object,
-        basis: The dimension reduction method utilized. Defaults to "umap".
-        transition_method: The method to calculate the transition matrix and project high dimensional vector to low
-            dimension.
-        maxIter: The max iteration numbers. Defaults to 10.
-        sigma: The bandwidth parameter. Defaults to 0.001.
-        Lambda: The regularization parameter for inverse graph embedding. Defaults to None.
-        gamma: The regularization parameter for k-means. Defaults to 10.
-        ncenter: The number of centers to be considered. If None, number of centers would be calculated automatically.
-            Defaults to None.
-        raw_embedding: Whether to project the nodes on the principal graph into the original embedding. Defaults to
-            True.
-
-    Raises:
-        Exception: invalid `basis`.
-        Exception: adata.uns["transition_matrix"] not defined.
-
-    Returns:
-        An updated AnnData object that is updated with principal_g_transition, X__DDRTree and X_DDRTree_pg keys.
-    """
-
-    X = adata.obsm["X_" + basis] if "X_" + basis in adata.obsm.keys() else None
-    if X is None:
-        raise Exception("{} is not a key of obsm ({} dimension reduction is not performed yet.).".format(basis, basis))
-
-    transition_matrix = (
-        adata.obsp[transition_method + "_transition_matrix"]
-        if transition_method + "_transition_matrix" in adata.obsp.keys()
-        else None
-    )
-    if transition_matrix is None:
-        raise Exception("transition_matrix is not a key of uns. Please first run cell_velocity.")
-
-    Lambda = 5 * X.shape[1] if Lambda is None else Lambda
-    ncenter = 250 if cal_ncenter(X.shape[1]) is None else ncenter
-
-    Z, Y, principal_g, cell_membership, W, Q, C, objs = DDRTree(
-        X,
-        maxIter=maxIter,
-        Lambda=Lambda,
-        sigma=sigma,
-        gamma=gamma,
-        ncenter=ncenter,
-    )
-
-    X = csr_matrix(principal_g)
-    Tcsr = minimum_spanning_tree(X)
-    principal_g = Tcsr.toarray().astype(int)
-
-    # here we can also identify siginificant links using methods related to PAGA
-    transition_matrix = transition_matrix.toarray()
-    principal_g_transition = cell_membership.T.dot(transition_matrix).dot(cell_membership) * principal_g
-
-    adata.uns["principal_g_transition"] = principal_g_transition
-    adata.obsm["X_DDRTree"] = X.T if raw_embedding else Z
-    cell_membership = csr_matrix(cell_membership)
-    adata.uns["X_DDRTree_pg"] = cell_membership.dot(X.T) if raw_embedding else Y
-
-    return adata
