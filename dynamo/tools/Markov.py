b8a7c027757ea864c4f1cdffdc8e0c6060b8cf14
sichao <sichao_y@outlook.com>
2023-12-01 15:24:22 -0500

    debug is_normalized in Markov

--- dynamo/tools/Markov.py
+++ dynamo/tools/Markov.py
@@ -758,7 +758,7 @@ class MarkovChain:
         Returns:
             True if the matrix is properly normalized.
         """
-        if not P:
+        if P is not None:
             main_warning("No transition matrix input. Normalization check is skipped.")
             return True
         else:
