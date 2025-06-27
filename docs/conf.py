8c798ed595e91212cd40cf0d53e1f008d6c9511c
Starlitnightly <starlitnightly@163.com>
2025-06-27 13:05:05 -0700

    Updated the theme of dynamo tutorial

--- docs/conf.py
+++ docs/conf.py
@@ -157,7 +157,8 @@ html_theme_options = {
     "path_to_docs": "docs/",
     "repository_branch": version,
 }
-pygments_style = "default"
+pygments_style = "tango"
+pygments_dark_style = "monokai"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
